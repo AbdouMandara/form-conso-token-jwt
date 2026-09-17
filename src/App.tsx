93e37246831af4d938c20fe566d481c053542c59
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 09:54:27 +0100

    Fix : Chargement du theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 function App() {
 
   return (
-    <>
-      <button className="btn btn-primary ">Click me</button>
-    </>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" className="input" placeholder="Password" />
+
+        <button className="btn btn-neutral mt-4">Se connecter</button>
+      </fieldset>
+    </div>
   )
 }
 
