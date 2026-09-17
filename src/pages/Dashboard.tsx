0fb9980edc6ac66bd174d5c9c3b4aa3f49c5c7d0
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 15:42:48 +0100

    Gestion de la recup reussit du user ou non

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react"
 import userConnecte from "../api/userConnecte"
 import { type User } from "../types/userConnecte"
 export default  function Dashboard(){
-    const [userFetched, setUserFetched] = useState<User>({} as User)
+    const [userFetched, setUserFetched] = useState<User|null>(null)
     useEffect(()=>{
         const fetchUser = async () => {
             try {
                 const user = await userConnecte(); //j'ai await car sinon ca devrait me retourner une promesse et je ne pourrais pas acceder aux données de l'utilisateur
-                setUserFetched(user);
+                setUserFetched(user)
             } catch (error) {
-                console.error("Erreur lors de la récupération de l'utilisateur :", error);
+                console.error("Erreur lors de la récupération de l'utilisateur :", error)
             }
         }
         fetchUser();
@@ -26,7 +26,11 @@ export default  function Dashboard(){
         </div>
 
         <div>
-            <p>Bienvenue, {userFetched.firstName} {userFetched.lastName}!</p>
+            {userFetched ? (
+                <p>Bienvenue, {userFetched.firstName} {userFetched.lastName}!</p>
+            ) : (
+                <p>Chargement...</p>
+            )}
         </div>
         </>
     )
