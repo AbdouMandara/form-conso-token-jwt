16c1068dbc6b06493466fad1aa68b58dfa15867a
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 11:07:14 +0100

    Refactor : UserSchema exporte | utilisation de RHF au niveau du formulaire

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import UserSchema from "./schemas/dataForm.schema"
 function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(UserSchema),
+    defaultValues: {
+      email: "a",
+      password: ""
+    }
+  })
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
 
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4" onSubmit={handleSubmit(onSubmit)}>
         <legend className="fieldset-legend">Login</legend>
 
         <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
+        <input {...register("email")} className="input" placeholder="Email" />
+        <p className="text-error">{errors.email?.message}</p>
 
-        <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
+        <label className="label">Mot de passe</label>
+        <input {...register("password")} className="input" placeholder="Password" />
+        <p className="text-error">{errors.password?.message}</p>
 
-        <button className="btn btn-neutral mt-4">Se connecter</button>
-      </fieldset>
+        <button className="btn btn-neutral mt-4" type="submit">Se connecter</button>
+      </form>
     </div>
   )
 }
--- src/schemas/dataForm.schema.ts
+++ src/schemas/dataForm.schema.ts
@@ -1,6 +1,8 @@
 import * as z from 'zod'
 
-const User =z.object({
+const UserSchema =z.object({
     email: z.email(),
-    password: z.string().min(8, { message: "Password must be at least 8 characters long" })
+    password: z.string().min(8, { message: "Le mot de passe doit avoir au moins 8 caractères" })
 })
+export default UserSchema;
+export type User = z.infer<typeof UserSchema>;
