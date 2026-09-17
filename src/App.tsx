ad7a60a817cc935b3081ca60752c70ec5770fb5c
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 11:51:00 +0100

    Refactor : mise en place de username dans le schema zod et le form

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: zodResolver(UserSchema),
     defaultValues: {
-      email: "",
+      username: "",
       password: ""
     }
   })
@@ -19,12 +19,12 @@ function App() {
       <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4" onSubmit={handleSubmit(onSubmit)}>
         <legend className="fieldset-legend">Login</legend>
 
-        <label className="label">Email</label>
-        <input {...register("email")} className="input" placeholder="Email" />
-        <p className="text-error">{errors.email?.message}</p>
+        <label className="label">Nom d'utilisateur</label>
+        <input {...register("username")} className="input" placeholder="Nom d'utilisateur" />
+        <p className="text-error">{errors.username?.message}</p>
 
         <label className="label">Mot de passe</label>
-        <input {...register("password")} className="input" placeholder="Password" />
+        <input type="password" {...register("password")} className="input" placeholder="Password" />
         <p className="text-error">{errors.password?.message}</p>
 
         <button className="btn btn-neutral mt-4" type="submit">Se connecter</button>
--- src/schemas/dataForm.schema.ts
+++ src/schemas/dataForm.schema.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod'
 
 const UserSchema =z.object({
-    email: z.string({error : 'Entrez une nom valide'}),
+    username: z.string({error : 'Entrez un nom valide'}),
     password: z.string().min(8, { message: "Le mot de passe doit avoir au moins 8 caractères" })
 })
 export default UserSchema;
