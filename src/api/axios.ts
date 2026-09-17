fef59f2a398874de00fea2175fe374c881618680
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 11:32:53 +0100

    Mise en place de l'instance axios - template

---
+++ src/api/axios.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import type { AxiosInstance } from "axios";
+
+const api: AxiosInstance = axios.create({
+  baseURL: "https://api.example.com",
+  timeout: 5000,
+});
+
+export default api;
