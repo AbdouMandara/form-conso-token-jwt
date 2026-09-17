92c5b6ddd285debf1821419843d6d5b43e6b4672
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 12:00:53 +0100

    Ajout du param de l'instance axios pour prendre en compte l'auth par cookie

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,7 @@ import type { AxiosInstance } from "axios";
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 5000,
+  withCredentials: true,
 });
 
 export default api;
