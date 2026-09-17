4abc51d1208afd35b1ff6a37ed44dd0b240b2303
Abdou Mandara <abdoumandara@gmail.com>
2026-09-17 12:46:46 +0100

    Creation de la page Dashboard

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+export default function Dashboard(){
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">LoginAbdou</a>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Deconnexion</a>
+            </div>
+        </div>
+    )
+}
